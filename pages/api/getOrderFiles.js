40cd9ee3febb1e19939952103bf42091d1ebfca3
Mehul Shekhada <94891873+mehulshekhada@users.noreply.github.com>
2024-05-27 21:53:02 +0530

    iso date

--- pages/api/getOrderFiles.js
+++ pages/api/getOrderFiles.js
@@ -16,8 +16,8 @@ export default async function handler(req, res) {
     {$match: {
       reason: 'PENDING',
       createdAt: {
-        $gte: moment(dateToFilter).tz('IST').startOf('day').toDate(),
-        $lte: moment(dateToFilter).tz('IST').endOf('day').toDate()
+        $gte: moment(dateToFilter).tz('IST').startOf('day').toISOString(),
+        $lte: moment(dateToFilter).tz('IST').endOf('day').toISOString()
       }
     }},
     {$group : {
