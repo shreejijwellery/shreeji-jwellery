303027b59fd7ea567484e8a32cafa704cd76ae5b
Mehul Shekhada <94891873+mehulshekhada@users.noreply.github.com>
2024-10-29 21:22:27 +0530

    on reload clear storage

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,21 @@ import '../styles/globals.css';
 import Layout from '../components/Layout';
 import ToastContainerWrapper from '../components/ToastContainer';
 import 'tailwindcss/tailwind.css';
+import { useEffect } from 'react';
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const clearLocalStorage = () => {
+      localStorage.removeItem('items');
+      localStorage.removeItem('workers');
+      localStorage.removeItem('sections');
+    };
+
+    window.addEventListener('beforeunload', clearLocalStorage);
+
+    return () => {
+      window.removeEventListener('beforeunload', clearLocalStorage);
+    };
+  }, []);
   return (
     <Layout>
       <ToastContainerWrapper />
